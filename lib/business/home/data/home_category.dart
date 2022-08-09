16648ba5dd0051467a6191a2244e37030c29cd16
hongxiang <huxhu@thoughtworks.com>
2022-08-09 10:58:00 +0800

    [huhx] -feat: add simple home screen

---
+++ lib/business/home/data/home_category.dart
@@ -0,0 +1,10 @@
+enum HomeCategory {
+  home("首页"),
+  readRank("阅读榜"),
+  recommendRank("推荐榜"),
+  java("Java");
+
+  final String label;
+
+  const HomeCategory(this.label);
+}
--- lib/business/home/home_screen.dart
+++ lib/business/home/home_screen.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cnblog/business/home/home_category_screen.dart';
+import 'package:flutter_cnblog/component/appbar_back_button.dart';
 
-class HomeScreen extends StatefulWidget {
+import 'data/home_category.dart';
+
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: Text("Home screen"),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const TabBar(
+            tabs: [
+              Tab(text: "首页"),
+              Tab(text: "阅读榜"),
+              Tab(text: "推荐榜"),
+              Tab(text: "Java"),
+            ],
+            indicatorColor: Colors.white,
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            HomeCategoryScreen(category: HomeCategory.home),
+            HomeCategoryScreen(category: HomeCategory.readRank),
+            HomeCategoryScreen(category: HomeCategory.recommendRank),
+            HomeCategoryScreen(category: HomeCategory.java),
+          ],
+        ),
+      ),
     );
   }
 }
