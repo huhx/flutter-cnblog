eaca882806f09e22eba9cf5022ac1efc250bbcdf
hongxiang <huxhu@thoughtworks.com>
2022-08-09 09:46:20 +0800

    [huhx] -feat: add some package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
