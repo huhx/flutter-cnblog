a18badc0aa3317d091cd89838679936869649767
hongxiang <huxhu@thoughtworks.com>
2022-08-10 08:03:48 +0800

    [huhx] -feat: add context extension method

---
+++ lib/common/extension/context_extension.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+extension ContextExtensions on BuildContext {
+  Future<T?> goto<T extends Object?>(Widget widget) async {
+    return Navigator.push(this, MaterialPageRoute<T>(builder: (_) => widget));
+  }
+
+  void pop<T extends Object?>([T? result]) {
+    Navigator.pop<T>(this, result);
+  }
+}
