b56361b84037c0bb41fbd0957f14cc701949ac97
hongxiang <huxhu@thoughtworks.com>
2022-08-17 08:44:43 +0800

    [huhx] -refactor: use queryParameter in InstantApi

--- lib/api/instant_api.dart
+++ lib/api/instant_api.dart
@@ -5,17 +5,27 @@ import 'package:flutter_cnblog/model/instant.dart';
 import 'package:flutter_cnblog/util/dio_util.dart';
 
 class InstantApi {
+  static const String url = "https://ing.cnblogs.com/ajax/ing/GetIngList";
+
   Future<List<InstantInfo>> getAllInstants(InstantCategory category, int pageKey) async {
-    final String url =
-        "https://ing.cnblogs.com/ajax/ing/GetIngList?IngListType=${category.url}&PageIndex=$pageKey&_=${DateTime.now().microsecondsSinceEpoch}";
-    final Response response = await RestClient.withCookie().get(url);
+    final Map<String, dynamic> queryParameter = {
+      "IngListType": category.url,
+      "PageIndex": pageKey,
+      "_": DateTime.now().microsecondsSinceEpoch
+    };
+    final Response response = await RestClient.withCookie().get(url, queryParameters: queryParameter);
+
     return compute(InstantParser.parseInstantList, response.data as String);
   }
 
   Future<List<InstantInfo>> getMyInstants(MyInstantCategory category, int pageKey) async {
-    final String url =
-        "https://ing.cnblogs.com/ajax/ing/GetIngList?IngListType=${category.url}&PageIndex=$pageKey&_=${DateTime.now().microsecondsSinceEpoch}";
-    final Response response = await RestClient.withCookie().get(url);
+    final Map<String, dynamic> queryParameter = {
+      "IngListType": category.url,
+      "PageIndex": pageKey,
+      "_": DateTime.now().microsecondsSinceEpoch
+    };
+    final Response response = await RestClient.withCookie().get(url, queryParameters: queryParameter);
+
     return compute(InstantParser.parseInstantList, response.data as String);
   }
 }
