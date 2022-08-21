d288bebc92520c5b3f260ba06b104eeb9d0a0c91
hongxiang <gohuhx@163.com>
2022-08-21 21:15:08 +0800

    [huhx] -feat: add bookmark add & update & delete api

--- lib/api/bookmark_api.dart
+++ lib/api/bookmark_api.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_cnblog/model/bookmark.dart';
 import 'package:flutter_cnblog/model/bookmark_resp.dart';
+import 'package:flutter_cnblog/model/follow.dart';
 import 'package:flutter_cnblog/util/dio_util.dart';
 import 'package:retrofit/retrofit.dart';
 
@@ -11,6 +13,27 @@ abstract class BookmarkApi {
 
   @GET("/Bookmarks")
   Future<List<BookmarkResp>> getBookmarks(@Query("pageIndex") int pageIndex, @Query("pageSize") int pageSize);
+
+  Future<FollowResult> add(BookmarkRequest request) async {
+    const String url = "https://wz.cnblogs.com/api/wz";
+    final Response response = await RestClient.withCookie().post(url, data: request);
+
+    return FollowResult.fromJson(response.data);
+  }
+
+  Future<FollowResult> update(BookmarkRequest request) async {
+    const String url = "https://wz.cnblogs.com/api/wz";
+    final Response response = await RestClient.withCookie().put(url, data: request);
+
+    return FollowResult.fromJson(response.data);
+  }
+
+  Future<FollowResult> delete(String wzLinkId) async {
+    final String url = "https://wz.cnblogs.com/api/wz/$wzLinkId";
+    final Response response = await RestClient.withCookie().delete(url);
+
+    return FollowResult.fromJson(response.data);
+  }
 }
 
 final bookmarkApi = BookmarkApi(RestClient.getInstance());
--- lib/model/bookmark.dart
+++ lib/model/bookmark.dart
@@ -18,3 +18,36 @@ class BookmarkInfo extends Equatable {
   @override
   List<Object?> get props => [id, title, url, starCounts, postDate];
 }
+
+class BookmarkRequest {
+  final String wzLinkId;
+  final String url;
+  final String title;
+  final String tags;
+  final String summary;
+
+  const BookmarkRequest({
+    required this.wzLinkId,
+    required this.url,
+    required this.title,
+    this.tags = "",
+    this.summary = "",
+  });
+}
+
+class BookmarkResult extends Equatable {
+  final bool success;
+  final String message;
+
+  const BookmarkResult({required this.success, required this.message});
+
+  factory BookmarkResult.fromJson(Map<String, dynamic> json) {
+    return BookmarkResult(
+      success: json['success'] as bool,
+      message: json['message'] as String,
+    );
+  }
+
+  @override
+  List<Object?> get props => [success, message];
+}
