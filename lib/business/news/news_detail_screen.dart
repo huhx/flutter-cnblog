40b838bb7965317219d34d4db159aa69e0bfda3e
hongxiang <gohuhx@163.com>
2022-08-23 13:42:10 +0800

    [huhx] -feat: add bookmark feature in news detail

--- lib/business/news/news_detail_screen.dart
+++ lib/business/news/news_detail_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cnblog/api/bookmark_api.dart';
 import 'package:flutter_cnblog/component/appbar_back_button.dart';
 import 'package:flutter_cnblog/component/center_progress_indicator.dart';
+import 'package:flutter_cnblog/model/bookmark.dart';
 import 'package:flutter_cnblog/model/news.dart';
 import 'package:flutter_cnblog/util/app_config.dart';
+import 'package:flutter_cnblog/util/comm_util.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class NewsDetailScreen extends StatefulWidget {
@@ -21,8 +24,38 @@ class _NewsDetailScreenState extends State<NewsDetailScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.news.title),
+        centerTitle: false,
+        title: Text(widget.news.submitter),
         leading: const AppbarBackButton(),
+        actions: [
+          FutureBuilder(
+            future: bookmarkApi.isMark(widget.news.httpsUrl().toString()),
+            builder: (context, snap) {
+              if (!snap.hasData) return const SizedBox();
+              bool isMark = snap.data as bool;
+              return StatefulBuilder(
+                builder: (context, setter) {
+                  return Visibility(
+                    visible: !isMark,
+                    child: TextButton(
+                      onPressed: () async {
+                        final BookmarkRequest request = BookmarkRequest(
+                          wzLinkId: widget.news.id,
+                          url: widget.news.httpsUrl().toString(),
+                          title: widget.news.title,
+                        );
+                        await bookmarkApi.add(request);
+                        setter(() => isMark = !isMark);
+                        CommUtil.toast(message: "收藏成功！");
+                      },
+                      child: const Text("收藏", style: TextStyle(color: Colors.white)),
+                    ),
+                  );
+                },
+              );
+            },
+          )
+        ],
       ),
       body: Stack(
         children: [
