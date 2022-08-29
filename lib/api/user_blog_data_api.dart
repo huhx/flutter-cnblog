ad47a2e7981fd13e764fa313d17d444c91c6ade5
hongxiang <gohuhx@163.com>
2022-08-29 20:43:32 +0800

    [huhx] -feat: get blog stats with cooke

--- lib/api/user_blog_data_api.dart
+++ lib/api/user_blog_data_api.dart
@@ -2,11 +2,12 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_cnblog/common/parser/blog_data_parser.dart';
 import 'package:flutter_cnblog/model/user_blog.dart';
+import 'package:flutter_cnblog/util/dio_util.dart';
 
 class UserBlogDataApi {
   Future<BlogDataInfo> getBlogDataInfo(String name) async {
     final String url = "https://www.cnblogs.com/$name/ajax/blogStats";
-    final Response response = await Dio().get(url);
+    final Response response = await RestClient.withCookie().get(url);
 
     return compute(BlogDataParser.parseBlogDataList, response.data as String);
   }
