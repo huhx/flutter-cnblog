a38fa0f77f4a1ea7ce686bd91b948832f01352c3
hongxiang <gohuhx@163.com>
2022-10-10 15:37:05 +0800

    [huhx] -feat: add test for the getCodeFromUrl method from AuthRequest

---
+++ test/common/constant/auth_request_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_cnblog/common/constant/auth_request.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test("get authorization code from url", () {
+    const String url = "https://oauth.cnblogs.com/auth/callback#code=D3A104A979E6221C7C45E8CA&id_token=eyJhbGciOiJSUzI1NiIsImtpZCI6";
+
+    final String result = AuthRequest.getCodeFromUrl(url);
+
+    expect(result, "D3A104A979E6221C7C45E8CA");
+  });
+}
