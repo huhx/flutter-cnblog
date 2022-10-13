b8ed22907ec7f396e640fca199ea2499eaf1ea45
hongxiang <gohuhx@163.com>
2022-10-13 19:33:12 +0800

    [huhx] -feat: add login logic in official blog when review news detail

--- lib/business/profile/official/official_blog_review_screen.dart
+++ lib/business/profile/official/official_blog_review_screen.dart
@@ -3,11 +3,15 @@ import 'package:flutter_cnblog/api/official_blog_api.dart';
 import 'package:flutter_cnblog/business/home/blog_detail_screen.dart';
 import 'package:flutter_cnblog/business/news/news_detail_screen.dart';
 import 'package:flutter_cnblog/business/profile/user_profile_detail_screen.dart';
+import 'package:flutter_cnblog/business/user/data/session_provider.dart';
+import 'package:flutter_cnblog/business/user/login/login_screen.dart';
 import 'package:flutter_cnblog/common/extension/context_extension.dart';
 import 'package:flutter_cnblog/component/appbar_back_button.dart';
 import 'package:flutter_cnblog/component/center_progress_indicator.dart';
 import 'package:flutter_cnblog/component/list_tile_trailing.dart';
 import 'package:flutter_cnblog/model/official_blog.dart';
+import 'package:flutter_cnblog/model/user.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class OfficialBlogReviewScreen extends StatelessWidget {
   final OfficialBlog blog;
@@ -79,18 +83,28 @@ class HotBlogList extends StatelessWidget {
   }
 }
 
-class HostNewsList extends StatelessWidget {
+class HostNewsList extends ConsumerWidget {
   final List<OfficialHot> newsList;
 
   const HostNewsList(this.newsList, {Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final User? user = ref.watch(sessionProvider);
+
     return ListView.builder(
       itemCount: newsList.length,
       itemBuilder: (context, index) {
         return InkWell(
-          onTap: () => context.goto(NewsDetailScreen(newsList[index].toDetail())),
+          onTap: () async {
+            if (user == null) {
+              final bool? isSuccess = await context.gotoLogin(const LoginScreen());
+              if (isSuccess == null) {
+                return;
+              }
+            }
+            context.goto(NewsDetailScreen(newsList[index].toDetail()));
+          },
           child: Card(
             child: Container(
               padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
