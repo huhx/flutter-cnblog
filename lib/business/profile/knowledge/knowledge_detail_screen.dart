8d63ddd7c7e58b2cb46e52f5eea298f859367ab7
hongxiang <gohuhx@163.com>
2022-10-13 10:47:41 +0800

    [huhx] -fix: the issue can not navigate when not login in KnowledgeDetailScreen

--- lib/business/profile/knowledge/knowledge_detail_screen.dart
+++ lib/business/profile/knowledge/knowledge_detail_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cnblog/api/bookmark_api.dart';
 import 'package:flutter_cnblog/api/html_css_api.dart';
 import 'package:flutter_cnblog/business/home/blog_share_screen.dart';
+import 'package:flutter_cnblog/business/user/data/session_provider.dart';
+import 'package:flutter_cnblog/business/user/login/login_screen.dart';
 import 'package:flutter_cnblog/common/constant/content_type.dart';
 import 'package:flutter_cnblog/common/extension/context_extension.dart';
 import 'package:flutter_cnblog/component/appbar_back_button.dart';
@@ -9,18 +11,21 @@ import 'package:flutter_cnblog/component/center_progress_indicator.dart';
 import 'package:flutter_cnblog/component/svg_action_icon.dart';
 import 'package:flutter_cnblog/model/blog_share.dart';
 import 'package:flutter_cnblog/model/detail_model.dart';
+import 'package:flutter_cnblog/model/user.dart';
 import 'package:flutter_cnblog/theme/shape.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
-class KnowledgeDetailScreen extends HookWidget {
+class KnowledgeDetailScreen extends HookConsumerWidget {
   final DetailModel knowledge;
 
   const KnowledgeDetailScreen(this.knowledge, {super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final User? user = ref.watch(sessionProvider);
     final isLoading = useState(true);
 
     return Scaffold(
@@ -31,6 +36,12 @@ class KnowledgeDetailScreen extends HookWidget {
           IconButton(
             icon: const SvgActionIcon(name: "more_hor"),
             onPressed: () async {
+              if (user == null) {
+                final bool? isSuccess = await context.gotoLogin(const LoginScreen());
+                if (isSuccess == null) {
+                  return;
+                }
+              }
               final bool isMark = await bookmarkApi.isMark(knowledge.url);
               final BlogShareSetting setting = BlogShareSetting(isMark: isMark, isDarkMode: context.isDarkMode());
 
