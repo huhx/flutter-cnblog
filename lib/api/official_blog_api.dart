7705d9d96198ebecaa4bd209f5a0b0da6882e719
hongxiang <gohuhx@163.com>
2022-11-24 22:07:39 +0800

    [huhx] -fix: view official blog without login

--- lib/api/official_blog_api.dart
+++ lib/api/official_blog_api.dart
@@ -1,19 +1,20 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_cnblog/common/constant/token_type.dart';
 import 'package:flutter_cnblog/common/parser/official_blog_parser.dart';
 import 'package:flutter_cnblog/model/official_blog.dart';
 import 'package:flutter_cnblog/util/dio_util.dart';
 
 class OfficialBlogApi {
   Future<List<OfficialBlog>> getOfficialBlogs(int pageKey) async {
-    final Dio dio = RestClient.withCookie();
+    final Dio dio = RestClient.getInstance(tokenType: TokenType.none);
     final Response response = await dio.get("https://www.cnblogs.com/cmt/default.html?page=$pageKey");
 
     return compute(OfficialBlogParser.parseOfficialBlogList, response.data as String);
   }
 
   Future<OfficialHotResponse> getHotList(String url) async {
-    final Response response = await RestClient.withCookie().get(url);
+    final Response response = await RestClient.getInstance(tokenType: TokenType.none).get(url);
 
     return compute(OfficialBlogParser.parseOfficialHotList, response.data as String);
   }
