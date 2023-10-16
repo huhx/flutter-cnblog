fb0f4e0ee5fec4b9ef889314c357db5d9cf667d8
hongxiang <gohuhx@163.com>
2023-10-16 10:24:17 +0800

    [huhx] -feat: fix the env type issue

--- lib/common/constant/env.dart
+++ lib/common/constant/env.dart
@@ -5,11 +5,11 @@ part 'env.g.dart';
 @Envied(path: '.env')
 abstract class Env {
   @EnviedField(varName: 'clientId', obfuscate: true)
-  static final clientId = _Env.clientId;
+  static final String clientId = _Env.clientId;
 
   @EnviedField(varName: 'clientSecret', obfuscate: true)
-  static final clientSecret = _Env.clientSecret;
+  static final String clientSecret = _Env.clientSecret;
 
   @EnviedField(varName: 'notRobotCookie', obfuscate: true)
-  static final notRobotCookie = _Env.notRobotCookie;
+  static final String notRobotCookie = _Env.notRobotCookie;
 }
